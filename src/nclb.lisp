f17d249b2d932f975815fbe74cbd6013933ea3aa
Greg Pfeil <greg@technomadic.org>
2012-07-08 15:04:40 -0600

    Minor changes to the docs.

--- src/nclb.lisp
+++ src/nclb.lisp
@@ -9,7 +9,11 @@ This is _NCLB_ (No Coder Left Behind), a literate programming system. It is
 less like _CWEB_ than anything I've seen. The _CWEB_ manual says “[w]riting
 CWEB programs is something like writing TEX documents, but with an additional
 ‘C mode’ that is added to TEX’s horizontal mode, vertical mode, and math mode.”
-In contrast, _NCLB_ removes the idea of a “web” and all files are either valid documentation files or valid code files, with the operations `tangle` and `weave` simply converting one to the other. In a code file, the documentation is contained in specially-marked comments and in a documentation file, the code is in designated code sections. There are a few reasons for this:
+In contrast, _NCLB_ removes the idea of a “web” and all files are either valid
+documentation files or valid code files, with the operations `tangle` and `weave`
+simply converting one to the other. In a code file, the documentation is contained in
+specially-marked comments and in a documentation file, the code is in designated code
+sections. There are a few reasons for this:
 
 1. with the documentation in comments, there is no need for the `tangle`
    operation, since the original file is executable (this is especially useful
@@ -47,19 +51,30 @@ in an order the compiler can process. However, macros can rearrange sections
 during the weaving process, so it is hopefully not too big of a problem to
 overcome.
 
-;@@ merge this in somewhere earlier
+<!-- merge this in somewhere earlier -->
 
-Literate programming is useful to people who are reading the code – either developers working on it or users learning from it. In the former case, less is better, because those who change the code often ignore the docs while those who try to understand the system often ignore the code – if the two aren't in sync, trouble brews.
+Literate programming is useful to people who are reading the code – either developers
+working on it or users learning from it. In the former case, less is better, because
+those who change the code often ignore the docs while those who try to understand the
+system often ignore the code – if the two aren't in sync, trouble brews.
 
-Documentation for users of a system belong in a combination of docstrings and literate tutorials & test cases. E.g., this literate document is part of a tutorial on _NCLB_, not part of the _NCLB_ source code. Tutorials and the like benefit from literate programming because the code sections can be compiled and tested and the text sections will be consumed as the primary content and potentially checked by editors.
+Documentation for users of a system belong in a combination of docstrings and literate
+tutorials & test cases. E.g., this literate document is part of a tutorial on _NCLB_,
+not part of the _NCLB_ source code. Tutorials and the like benefit from literate
+programming because the code sections can be compiled and tested and the text sections
+will be consumed as the primary content and potentially checked by editors.
 
-Okay, so there _is_ a `tangle`, but it’s not used on the same original file. `tangle` converts valid documentation to valid code. There is still no “web” file, there are only code and documentation files. `weave` converts from code to documentation and `tangle` converts from documentation to code. At least in their current state, they are not isomorphic, but it does seem desirable that
+Okay, so there _is_ a `tangle`, but it’s not used on the same original file. `tangle`
+converts valid documentation to valid code. There is still no “web” file, there are
+only code and documentation files. `weave` converts from code to documentation and
+`tangle` converts from documentation to code. At least in their current state, they
+are not isomorphic, but it does seem desirable that
 
 > tangle(weave(x)) = x
 
 However, there is some normalization that happens, so it is better to expect that
 
-> weave(tangle(weave(x))) = weave(x)
+> tangle(weave(tangle(x))) = tangle(x)
 
 to compensate for that.
 @|#
